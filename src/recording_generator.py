3dac3d6b6e13dab3c046adda00e053d03a493c1e
Anna Reisz <reiszanna08@gmail.com>
2024-10-28 09:48:45 +0100

    src can be imported parallelly if repo installed via pip install -e .

---
+++ src/recording_generator.py
@@ -0,0 +1,275 @@
+import numpy as np
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+
+
+class RecordingGenerator():
+    def __init__(self, 
+                 first_AP_stim_lambda_ms = 1, # Exponential
+                 AP_length_mean_std_ms = [2, 0.1], # Gaussian
+                 AP_amplitude_mean_std_pct = [1, 1], # Gaussian
+                 SA_amplitude_mean_std_pct = [1, 0.1], # Gaussian
+                 num_cells = 10, # Poisson # num_spike trains
+                 spike_train_start_lambda_ms = 1, # Exponential # first spike after begining of SA
+                 spike_train_rate_lambda = 1, # Poisson # number of spikes per spike train
+                 inter_spike_train_interval_lambda_ms = 1, # Exponential
+                 CAP_jitter_mean_std_ms = [1, 0.1], # Gaussian
+                 template_jitter_ms = 0.01, # Uniform (width)
+                 SA_templates = 'SA_templates.npy',
+                 AP_templates = 'AP_templates.npy',
+                 ME_template  = 'ME_template.npy',
+                 ):
+        self.first_AP_stim_lambda_ms = first_AP_stim_lambda_ms
+        self.AP_length_mean_std_ms = AP_length_mean_std_ms
+        self.AP_amplitude_mean_std_pct = AP_amplitude_mean_std_pct
+        self.SA_amplitude_mean_std_pct = SA_amplitude_mean_std_pct
+        self.num_cells = num_cells
+        self.spike_train_start_lambda_ms = spike_train_start_lambda_ms
+        self.spike_train_rate_lambda = spike_train_rate_lambda
+        self.inter_spike_train_interval_lambda_ms = inter_spike_train_interval_lambda_ms
+        self.CAP_jitter_mean_std_ms = CAP_jitter_mean_std_ms
+        self.template_jitter_ms = template_jitter_ms
+
+        self.fs = 30_000 # 30 kHz
+
+        self.SA_templates = self._set_template(SA_templates) # 300 points, 30 kHz
+        print(self.SA_templates.shape)
+        self.AP_templates = self._set_template(AP_templates) # variable sampling rate
+        print(self.AP_templates.shape)
+        self.ME_template  = self._set_template(ME_template)  # 600 points, 30 kHz   
+        print(self.ME_template.shape)     
+
+        # should start with 0 and end with 0
+        self.AP_templates = np.concatenate([np.zeros((len(self.AP_templates), 1)), 
+                                            self.AP_templates, 
+                                            np.zeros((len(self.AP_templates), 1))], axis=1)
+        
+        self.SA_length = self.SA_templates.shape[1]
+        self.AP_length = self.AP_templates.shape[1]
+
+    def _set_template(self, template):
+        if isinstance(template, str):
+            return np.load(template)
+        else:
+            return template        
+
+    def interp_template(self, template, template_length_ms):
+        time_old_ms = np.linspace(0, template_length_ms, len(template))
+        
+        num_points = int(self.fs*template_length_ms//1000)
+        random_jitter = np.random.uniform(-self.template_jitter_ms/2, self.template_jitter_ms/2)
+        time_new_ms = np.linspace(self.template_jitter_ms/2+random_jitter, 
+                                  template_length_ms-self.template_jitter_ms/2 + random_jitter, 
+                                  num_points)
+        interp_func = interp1d(time_old_ms, template, kind='cubic')
+        try:
+            interp_template = interp_func(time_new_ms)
+        except Exception as e:
+            print(template_length_ms)
+            print(self.template_jitter_ms)
+            print(random_jitter)
+            print(time_old_ms)
+            print(time_new_ms)
+            interp_template = interp_func(time_new_ms)
+        
+        return interp_template, time_new_ms
+    
+    def generate(self, num_stimuli=1,verbose=0):
+        """
+        Generates a recording with num_stimuli stimuli
+
+        Returns:
+        - values: stimuli and spikes
+        - indexes: indexes of the values in the data array
+        """
+        segment_length=3000
+
+        data = np.zeros(num_stimuli*segment_length)
+        is_spike = np.zeros(num_stimuli*segment_length)
+        amount_spikes = np.zeros(num_stimuli*segment_length)
+
+        SAs, APs, SA_indexes, AP_indexes = [], [], [], []
+        SA_template = self.SA_templates[np.random.choice(len(self.SA_templates)), :]
+
+        for stimulus_idx in range(num_stimuli):
+            if verbose: print(f"Stimulus {stimulus_idx}")
+            
+            SA, _ = self.interp_template(SA_template, 10) # add time jitter
+            SA_amplitude = np.random.normal(*self.SA_amplitude_mean_std_pct)
+            SA *= SA_amplitude
+            
+            start_SA = stimulus_idx*segment_length
+            end_SA = start_SA + self.SA_length
+            idxs = np.arange(start_SA, end_SA)
+            data[idxs[0]: idxs[-1]+1] += SA
+
+            SAs.append(SA)
+            SA_indexes.append(idxs)
+
+            num_cells = np.random.poisson(self.num_cells)
+            for cell_idx in range(num_cells):
+                if verbose: print(f"Spike train {cell_idx}")
+
+                start_idx = int(np.ceil(np.random.exponential(self.spike_train_start_lambda_ms) / 1000 * self.fs) + end_SA)
+                num_spikes = np.random.poisson(self.spike_train_rate_lambda)
+                AP_template = self.AP_templates[np.random.choice(len(self.AP_templates)), :]
+
+                for spike_idx in range(num_spikes):
+                    if verbose: print(f"Spike {spike_idx}")
+
+                    AP_template_length_ms = np.random.normal(*self.AP_length_mean_std_ms)
+                    AP_template_length_ms = np.max([AP_template_length_ms, self.template_jitter_ms*2])
+                    AP, _ = self.interp_template(AP_template, AP_template_length_ms)
+                    AP_amplitude = np.random.normal(*self.AP_amplitude_mean_std_pct)
+                    AP *= AP_amplitude
+
+                    if start_idx + len(AP) < num_stimuli*segment_length:
+                        data[start_idx: start_idx + len(AP)] += AP
+                        is_spike[start_idx: start_idx + len(AP)] = 1
+                        amount_spikes[start_idx: start_idx + len(AP)] += 1
+
+                    APs.append(AP)
+                    AP_indexes.append(np.arange(start_idx, start_idx + len(AP)))
+
+                    start_idx += len(AP)
+
+                    # TOASK: make it fx beta so that intervals are more alike?
+                    inter_spike_train_time = np.random.exponential(self.inter_spike_train_interval_lambda_ms) 
+                    inter_spike_train_idx_offset = int(inter_spike_train_time / 1000 * self.fs)
+                    start_idx += inter_spike_train_idx_offset
+
+        return SAs, SA_indexes, APs, AP_indexes, is_spike, amount_spikes, data
+    
+    def add_white_noise(self, data, SNR_dB=10):
+        """
+        Adds white noise to the data with a given SNR in dB
+        """
+        signal_power = np.mean(data**2)
+        noise_power = signal_power / 10**(SNR_dB/10)
+        noise = np.random.normal(0, np.sqrt(noise_power), len(data))
+        return data + noise
+    
+    def add_mains_electricity_noise(self, data, ME_template=None, SNR_dB=10):
+        """
+        Adds mains electricity noise to the data
+        """
+        # TODO: interpolate ME_template
+        if ME_template is None:
+            ME_template = self.ME_template
+        ME = np.tile(ME_template.flatten(), len(data)//len(ME_template) + 1)
+        ME = ME[:len(data)]
+        # amplify to match SNR
+        ME_power = np.mean(ME**2)
+        data_power = np.mean(data**2)
+        ME *= np.sqrt(data_power / ME_power / 10**(SNR_dB/10))
+        # random phase
+        ME = np.roll(ME, np.random.randint(len(ME)))
+        
+        return data + ME
+    
+    def add_spontaneous_spikes(self, data, firing_Hz=1):
+        """
+        Adds spontaneous spikes to the data
+        """
+        num_spikes = np.random.poisson(firing_Hz * len(data) / self.fs)
+        for _ in range(num_spikes):
+            AP_template = self.AP_templates[np.random.choice(len(self.AP_templates)), :]
+            AP_template_length_ms = np.random.normal(*self.AP_length_mean_std_ms)
+            AP, _ = self.interp_template(AP_template, AP_template_length_ms)
+            AP_amplitude = np.random.normal(*self.AP_amplitude_mean_std_pct)
+            AP *= AP_amplitude
+            start_idx = np.random.randint(len(data) - len(AP))
+            data[start_idx: start_idx + len(AP)] += AP
+        return data
+
+if __name__ == "__main__":
+                
+    rec = RecordingGenerator(
+        first_AP_stim_lambda_ms = 0.2,
+        AP_length_mean_std_ms = [5, 1],
+        AP_amplitude_mean_std_pct = [10, 0.5],
+        SA_amplitude_mean_std_pct = [1, 0.1],
+        num_cells = 50,
+        spike_train_start_lambda_ms = 1,
+        spike_train_rate_lambda = 3,
+        inter_spike_train_interval_lambda_ms = 5,
+        CAP_jitter_mean_std_ms = [1, 0.1],
+        template_jitter_ms = 1, 
+        )
+    
+    SAs, SA_indexes, APs, AP_indexes, is_spike, amount_spike, data = rec.generate(3, verbose=0)
+    noised_data = rec.add_white_noise(data, SNR_dB=20)
+    noised_data = rec.add_mains_electricity_noise(noised_data, SNR_dB=20)
+    noised_data = rec.add_spontaneous_spikes(noised_data, firing_Hz=1000)
+
+    plt.plot(noised_data, label='noised_data')
+    plt.plot(data, label='data', color='orange')
+    plt.legend()
+    plt.show()
+
+    plt.scatter(np.arange(len(data)), data, color=['olive' if x else 'pink' for x in is_spike], s=3)
+    plt.title('Data without noise, spiked index marked green')
+    plt.show()
+
+    plt.plot(amount_spike)
+    plt.title('Amount of spikes present at an index')
+    plt.show()
+
+    # order APs and AP_indexes by start index in AP_indexes
+
+    #order = np.argsort([idx[0] for idx in AP_indexes])
+    #APs = [APs[i] for i in order]
+    #AP_indexes = [AP_indexes[i] for i in order]
+    #AP_indexes_start = [idx[0] for idx in AP_indexes]
+    #AP_indexes_end = [idx[-1] for idx in AP_indexes]
+
+    # get samples for training 
+
+    fig, axs = plt.subplots(2, 1, figsize=(10, 10))
+
+    SA_ends = [idx[-1] for idx in SA_indexes]
+
+    SA_size = len(SA_indexes[0])
+    num_samples = 300
+    window_size = 200
+    window_offset = 50
+    X, y_class, y_reg = np.zeros((num_samples, window_size)), np.zeros(num_samples), np.zeros(num_samples)
+    i = 0
+    while i < num_samples:
+        for SA_end_idx in range(len(SA_ends)-1):
+            if i >= num_samples: break
+            window_start = SA_ends[SA_end_idx]
+
+            # while the window fits in this segment (so before the next stimulus)
+            while window_start < SA_ends[SA_end_idx+1]-SA_size-window_size:
+                if i >= num_samples: break
+                X[i] = noised_data[window_start:window_start+window_size]
+                # if more than 90% of the samples in this window are spikes then it is a spiking window
+                y_class[i] = is_spike[window_start: window_start+window_size].mean() > 0.90 
+                # count spikes fully contained in this window
+                y_reg[i] = len([x for x in AP_indexes if x[0] >= window_start and x[-1] <= window_start+window_size]) # TODO: this might be slow
+                if y_reg[i]:
+                    axs[0].plot(X[i], alpha=0.2, color='olive')
+                else:
+                    axs[1].plot(X[i], alpha=0.2, color='pink')
+                window_start += window_offset
+                i += 1
+
+    print("Samples found:", i)
+
+    axs[0].set_ylim(-800, 800)
+    axs[0].set_title('Spike')
+    axs[1].set_ylim(-800, 800)
+    axs[1].set_title('Not spike')
+    plt.show()
+
+    np.save('X.npy', X)
+    np.save('y_class.npy', y_class)
+    np.save('x_reg.npy', y_reg)
+    
+    plt.plot(y_reg)
+    plt.xlabel('Window id')
+    plt.ylabel('Num. fully contained spikes')
+    plt.show()
+        
+            
