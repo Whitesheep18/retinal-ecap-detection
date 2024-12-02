e79859153f51a87d9876b4250ca6ae75bc295fe3
Marie Lunde <mao.lunde@gmail.com>
2024-12-02 10:39:10 +0100

    add val-set for classification

--- src/train_and_eval.py
+++ src/train_and_eval.py
@@ -89,6 +89,7 @@ def train_and_eval_classification(model, dataset, results, save_model_path, verb
     if verbose: print(f"Training model {model}")
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
 
     model.fit(X_train, y_train)
     
