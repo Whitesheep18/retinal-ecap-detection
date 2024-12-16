3f496b778041d95c1296ea1cee23acdaef5f516c
Anna Reisz <reiszanna08@gmail.com>
2024-12-16 11:18:57 +0100

    init stride to batch

--- spike_detection/train_batch.sh
+++ spike_detection/train_batch.sh
@@ -3,5 +3,5 @@ for IDX in 1 3 5 7 9 11
 do
 python spike_detection/train.py --models AveragePrediction LinearRegression ThresholdBased DrCIFRegressor \
                                 --dataset_idx $IDX --results spike_detection/results.csv \
-                                --save_model_path models --comment "batch"
+                                --comment "batch" --init_stride 2
 done
