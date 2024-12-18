8cf0fd561d60136eb45c942d7b1b13b280e3e4d6
Anna Reisz <reiszanna08@gmail.com>
2024-12-18 11:21:06 +0100

    updated training routine

--- spike_detection/train_batch.sh
+++ spike_detection/train_batch.sh
@@ -1,6 +1,7 @@
 
-for IDX in 1 3 5 7 9 11
+for IDX in 2 4 6 8 10 12
 do
+echo "dataset idx $IDX"
 python spike_detection/train.py --models AveragePrediction LinearRegression ThresholdBased DrCIFRegressor \
                                 --dataset_idx $IDX --results spike_detection/results.csv \
                                 --comment "batch" --init_stride 2
